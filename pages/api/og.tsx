ac7dcd7a50023217ed0c3676dcc5b759c6a27e49
Lovell Liu <lovelliu@foxmail.com>
2023-01-25 14:38:28 +0800

    feat: add og api

---
+++ pages/api/og.tsx
@@ -0,0 +1,77 @@
+import type { NextRequest } from 'next/server'
+import { ImageResponse } from '@vercel/og'
+
+export const config = {
+  runtime: 'edge',
+}
+
+export default function handler(req: NextRequest) {
+  try {
+    const { searchParams } = new URL(req.url)
+
+    // ?title=<title>
+    const hasTitle = searchParams.has('title')
+    const title = hasTitle
+      ? searchParams.get('title')?.slice(0, 100)
+      : 'Lovell Liu'
+
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            backgroundColor: 'black',
+            backgroundSize: '150px 150px',
+            height: '100%',
+            width: '100%',
+            display: 'flex',
+            textAlign: 'center',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            flexWrap: 'nowrap',
+          }}
+        >
+          <div
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              justifyItems: 'center',
+            }}
+          >
+            <img
+              alt="Avatar"
+              height={200}
+              src="https://my-picture-bed-1304169582.cos.ap-nanjing.myqcloud.com/picture/avatar.jpg"
+              style={{ margin: '0 30px' }}
+              width={232}
+            />
+          </div>
+          <div
+            style={{
+              fontSize: 60,
+              fontStyle: 'normal',
+              letterSpacing: '-0.025em',
+              color: 'white',
+              marginTop: 30,
+              padding: '0 120px',
+              lineHeight: 1.4,
+              whiteSpace: 'pre-wrap',
+            }}
+          >
+            {title}
+          </div>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+      },
+    )
+  }
+  catch (e: any) {
+    return new Response('Failed to generate the image', {
+      status: 500,
+    })
+  }
+}
