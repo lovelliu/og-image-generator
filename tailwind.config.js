33b1304a1334c4e2ffbedb542702aff803bdfc3b
Lovell Liu <lovelliu@foxmail.com>
2023-01-28 12:10:24 +0800

    chore: add @tailwindcss/forms dep

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./pages/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+  plugins: [require('@tailwindcss/forms')],
 }
